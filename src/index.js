aec64b123fa81c551a4529c914c97efae7968ecd
Sergey Zubtsovskiy <sergey.zubtsovskiy@gmail.com>
2018-04-04 12:48:11 +0200

    Fix an issue with displaying JSON values after selecting them from history

--- src/index.js
+++ src/index.js
@@ -104,6 +104,12 @@ function app() {
                     const inputEvent = new Event('input');
                     inputEvent.targetValue = term;
                     input.dispatchEvent(inputEvent);
+                },
+                renderItem : (item, search) => {
+                    search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+                    const suggestionValue = item.replace(/"/g, '&quot;');
+                    const suggestionText = item.replace(new RegExp(`(${search.split(' ').join('|')})`, "gi"), "<b>$1</b>");
+                    return `<div class="autocomplete-suggestion" data-val="${suggestionValue}">${suggestionText}</div>`;
                 }
             });
         });
